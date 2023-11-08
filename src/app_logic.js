e25595df688c3a007c32f7474539116d83e96c73
Odi000 <mandimandi.ro@gmail.com>
2023-11-08 12:34:25 +0100

    wokring on hourly forecast section

--- src/app_logic.js
+++ src/app_logic.js
@@ -5,12 +5,15 @@ class WeatherApi {
     constructor() {
         this.data = null;
         this.search = async function (place) {
-            const response = await fetch(
-                `http://api.weatherapi.com/v1/forecast.json?key=d3effd02421b4d96a9d100840231309&q=${place}&days=3&aqi=no&alerts=no`
-            )
-            const parsedData = await response.json();
-            this.data = parsedData;
-            console.log(parsedData)
+            try {
+                const response = await fetch(
+                    `http://api.weatherapi.com/v1/forecast.json?key=d3effd02421b4d96a9d100840231309&q=${place}&days=3&aqi=no&alerts=no`
+                )
+                const parsedData = await response.json();
+                this.data = parsedData;
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 }
@@ -29,15 +32,15 @@ class UnitScale {
 }
 
 class HourlyForecast {
-    constructor(obj) {
-        this.data = obj.forecast.forecastday[0].hour;
+    constructor(obj, day = 0) {
+        this.data = obj.forecast.forecastday[day].hour;
     }
 }
 
 // Extracts only needed data from weather api
 class ExtractWeatherData {
     constructor(obj, day = 0) {
-        if(!day){
+        if (!day) {
             this.tempC = obj.current.temp_c;
             this.tempF = obj.current.temp_f;
             this.localTime = obj.location.localtime.split(' ')[1].split(':')[0];
@@ -58,19 +61,3 @@ export {
     HourlyForecast,
     ExtractWeatherData,
 }
-
-// Testing
-const weatherObj = new WeatherApi();
-const unitScale = new UnitScale();
-
-async function run() {
-    await weatherObj.search("brno");
-    const today = new ExtractWeatherData(weatherObj.data);
-    const tomorrow = new ExtractWeatherData(weatherObj.data, 1);
-    const afterTomorrow = new ExtractWeatherData(weatherObj.data, 2);
-    const hourlyData = new HourlyForecast(weatherObj.data);
-    console.table(today);
-    console.table(tomorrow);
-    console.table(afterTomorrow);
-}
-run();
--- src/index.js
+++ src/index.js
@@ -13,4 +13,70 @@ btn.src = magnify;
 
 const headerIcon = document.querySelector('.calendar>img');
 headerIcon.src = calendar;
-console.log()
+
+function DOMController() {
+    const weatherAPI = new WeatherApi();
+    const unitScale = new UnitScale();
+    const searchBar = document.querySelector('input');
+    const searchBtn = document.querySelector('button');
+
+    //Current-State Section
+    const locationName = document.querySelector('.location-name');
+    const currentCondition = document.querySelector('.condition');
+    const currentTemp = document.querySelector('.current-temp');
+    const currentHighest = document.querySelector('.highest');
+    const currentLowest = document.querySelector('.lowest');
+
+    searchBar.onkeydown = function (e) {
+        if (e.keyCode === 13) requestData(e.target.value);
+    }
+    searchBtn.onclick = () => {
+        requestData(searchBar.value);
+    };
+
+    async function requestData(value) {
+        await weatherAPI.search(value);
+        console.log(weatherAPI)
+        if (weatherAPI.data.error) {
+            return console.log(weatherAPI.data.error.message);
+        }
+
+        //Current-State Section Info to DOM
+        const neededData = new ExtractWeatherData(weatherAPI.data);
+        // unitScale.setChoice('f');
+
+        locationName.textContent = neededData.locationName;
+        currentCondition.textContent = neededData.condition;
+        if (unitScale.getChoice() === 'c') {
+            currentTemp.textContent = neededData.tempC + '°';
+            currentHighest.textContent = 'H: ' + neededData.maxTempC + '°';
+            currentLowest.textContent = 'L: ' + neededData.minTempC + '°';
+        } else {
+            currentTemp.textContent = neededData.tempF + '°';
+            currentHighest.textContent = 'H: ' + neededData.maxTempF + '°';
+            currentLowest.textContent = 'L: ' + neededData.minTempF + '°';
+        }
+
+        //Hourly Forecast Info to DOM
+        const hourlyForecast = new HourlyForecast(weatherAPI.data);
+        const nextDayHourlyForecast = new HourlyForecast(weatherAPI.data, 1);
+        const localTime = new Date(weatherAPI.data.location.localtime)
+        const hourNow = localTime.getHours();
+        const upcomingHours = hourlyForecast.data.filter(el => hourlyForecast.data.indexOf(el) >= hourNow);
+
+        if (upcomingHours.length > 12) console.log(true); // Maroje sot
+        if (upcomingHours.length < 12) {
+            let missingHours = 12 - upcomingHours.length;
+            for (let i = 0; i < missingHours; i++) {
+                upcomingHours.push(nextDayHourlyForecast.data[i]);
+            }
+        }
+
+        console.log(hourlyForecast)
+        console.log(nextDayHourlyForecast)
+        console.log(hourNow);
+        console.log(upcomingHours)
+    }
+}
+
+DOMController();
